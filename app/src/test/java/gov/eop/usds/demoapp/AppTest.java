baa8a18b641e0a53c14ad07cd50cb64a5d21d374
Brett Maden <github@maden.com>
2022-08-24 13:41:50 -0400

    Add divide operator and tests

--- app/src/test/java/gov/eop/usds/demoapp/AppTest.java
+++ app/src/test/java/gov/eop/usds/demoapp/AppTest.java
@@ -8,4 +8,16 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test public void divideWorks() {
+        final App app = new App();
+        Double result = app.divide(10., 2.);
+        assertEquals(Double.valueOf(5), result);
+    }
+
+    @Test public void divideThrows() {
+        final App app = new App();
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> app.divide(1., 0.));
+        assertEquals("divisor must not be zero", e.getMessage());
+    }
 }
