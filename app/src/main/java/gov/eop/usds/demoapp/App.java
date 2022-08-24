71c861804a36e5425ca50290c290eafd5a42bb2f
Brett Maden <github@maden.com>
2022-08-24 17:03:53 -0400

    apply formatting

--- app/src/main/java/gov/eop/usds/demoapp/App.java
+++ app/src/main/java/gov/eop/usds/demoapp/App.java
@@ -1,15 +1,15 @@
 package gov.eop.usds.demoapp;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public Integer add(final Integer a, final Integer b) {
-        return a + b;
-    }
+  public Integer add(final Integer a, final Integer b) {
+    return a + b;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
--- app/src/test/java/gov/eop/usds/demoapp/AppTest.java
+++ app/src/test/java/gov/eop/usds/demoapp/AppTest.java
@@ -1,11 +1,13 @@
 package gov.eop.usds.demoapp;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 }
