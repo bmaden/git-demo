9a2c58b6f5c138689d16587250128c02a2f8a079
Brett Maden <github@maden.com>
2022-09-09 12:14:06 +0000

    Add old log4j dependency (#11)
    * add log4j
    * configure dependabot

--- app/src/main/java/gov/eop/usds/demoapp/App.java
+++ app/src/main/java/gov/eop/usds/demoapp/App.java
@@ -1,6 +1,11 @@
 package gov.eop.usds.demoapp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+  private static final Logger log = LogManager.getLogger(App.class);
+
   public String getGreeting() {
     return "Hello World!";
   }
