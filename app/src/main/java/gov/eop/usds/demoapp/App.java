55a31ab3230dc5ffad2314d6bf8876988ce77a9f
Brett Maden <brett.m.maden@omb.eop.gov>
2022-09-09 08:04:50 -0400

    Bmaden usds/add spotless (#10)
    * add spotless
    * run checks automatically
    
    Co-authored-by: Brett Maden <github@maden.com>

--- app/src/main/java/gov/eop/usds/demoapp/App.java
+++ app/src/main/java/gov/eop/usds/demoapp/App.java
@@ -1,26 +1,26 @@
 package gov.eop.usds.demoapp;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public Integer add(final Integer a, final Integer b) {
-        return a + b;
-    }
+  public Integer add(final Integer a, final Integer b) {
+    return a + b;
+  }
 
-    public Integer subtract(final Integer a, final Integer b) {
-        return a - b;
-    }
+  public Integer subtract(final Integer a, final Integer b) {
+    return a - b;
+  }
 
-    public Double divide(final Double dividend, final Double divisor) {
-        if (divisor == 0.) {
-            throw new IllegalArgumentException("divisor must not be zero");
-        }
-        return dividend / divisor;
+  public Double divide(final Double dividend, final Double divisor) {
+    if (divisor == 0.) {
+      throw new IllegalArgumentException("divisor must not be zero");
     }
+    return dividend / divisor;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
--- app/src/test/java/gov/eop/usds/demoapp/AppTest.java
+++ app/src/test/java/gov/eop/usds/demoapp/AppTest.java
@@ -1,35 +1,42 @@
 package gov.eop.usds.demoapp;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 
-    @Test public void divideWorks() {
-        final App app = new App();
-        Double result = app.divide(10., 2.);
-        assertEquals(Double.valueOf(5), result);
-    }
+  @Test
+  public void divideWorks() {
+    final App app = new App();
+    Double result = app.divide(10., 2.);
+    assertEquals(Double.valueOf(5), result);
+  }
 
-    @Test public void divideThrows() {
-        final App app = new App();
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> app.divide(1., 0.));
-        assertEquals("divisor must not be zero", e.getMessage());
-    }
+  @Test
+  public void divideThrows() {
+    final App app = new App();
+    IllegalArgumentException e =
+        assertThrows(IllegalArgumentException.class, () -> app.divide(1., 0.));
+    assertEquals("divisor must not be zero", e.getMessage());
+  }
 
-    @Test public void simpleAddWorks() {
-        final App app = new App();
-        Integer sum = app.add(2, 2);
-        assertEquals(Integer.valueOf(4), sum);
-    }
+  @Test
+  public void simpleAddWorks() {
+    final App app = new App();
+    Integer sum = app.add(2, 2);
+    assertEquals(Integer.valueOf(4), sum);
+  }
 
-    @Test public void subtractWorks() {
-        final App app = new App();
-        Integer difference = app.subtract(10, 5);
-        assertEquals(Integer.valueOf(5), difference);
-    }
+  @Test
+  public void subtractWorks() {
+    final App app = new App();
+    Integer difference = app.subtract(10, 5);
+    assertEquals(Integer.valueOf(5), difference);
+  }
 }
